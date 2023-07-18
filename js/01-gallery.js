f37dfe90df6801b94d57d586ccb4d3d13f2ce5b4
Леонід Гембаровський <leongembar@gmail.com>
2023-07-19 02:45:59 +0300

    02

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,14 +1,15 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
 
+const containerEl = document.querySelector(".gallery");
 
-const containerEl = document.querySelector('.gallery');
-
-const string = galleryItems.map((item) =>
-    
-    `
+function makeStringGallegy(galleryItems) {
+  return galleryItems
+    .map(
+      (item) =>
+        `
     <li class="gallery__item">
         <a class="gallery__link" href="${item.original}">
             <img 
@@ -20,10 +21,53 @@ const string = galleryItems.map((item) =>
         </a>
     </li>
     `
-).join('');
+    )
+    .join("");
+}
 
-containerEl.innerHTML = string;
+containerEl.innerHTML = makeStringGallegy(galleryItems);
 
-const pictureEl = document.querySelector('.gallery__link');
+const pictureEl = document.querySelector(".gallery__link");
 
-pictureEl.addEventListener('click', ( (event)=> event.preventDefault() ) )
+pictureEl.addEventListener("click", (event) => event.preventDefault());
+
+containerEl.addEventListener("click", onShowModal);
+
+// let stringModal = "";
+
+// const instance = basicLightbox.create(stringModal);
+
+function onShowModal(e) {
+  const swatchEl = e.target;
+  //   swatchEl.closest(".gallery__link").preventDefault;
+  //   console.log(swatchEl.closest(".gallery__link"));
+
+  if (swatchEl.tagName === "IMG") {
+    const urlBigPhoto = swatchEl.dataset.source;
+    console.log(urlBigPhoto);
+    const instance = basicLightbox.create(`
+        <img 
+            class="gallery__image"
+            src="${urlBigPhoto}"
+        />
+     `);
+    // stringModal = `
+    //     <img
+    //         class="gallery__image"
+    //         src="${urlBigPhoto}"
+    //     />
+    // `;
+
+    instance.show();
+  }
+}
+
+document.addEventListener("keydown", onCloseModal);
+
+function onCloseModal(e) {
+  if (basicLightbox.visible()) {
+    if (e.code === "Escape") {
+      basicLightbox.close();
+    }
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const containerEl = document.querySelector(".gallery");
+
+function makeStringGallegy(galleryItems) {
+  return galleryItems
+    .map(
+      (item) =>
+        `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${item.original}">
+                <img class="gallery__image" 
+                     src="${item.preview}" 
+                     alt="${item.description}" 
+                />
+            </a>
+        </li>
+        `
+    )
+    .join("");
+}
+
+containerEl.innerHTML = makeStringGallegy(galleryItems);
